bd1d260ecba60cdcc79a878d0ee1b759d8b3cba5
Zhanibek <255004@astanait.edu.kz>
2026-04-26 17:23:39 +0500

    Added interactive update/delete logic to main.py

--- CampusFInd/main.py
+++ CampusFInd/main.py
@@ -1,4 +1,4 @@
-from CampusFInd.item import Item
+from item import Item
 from manager import LostFoundManager
 
 manager = LostFoundManager()
@@ -11,4 +11,18 @@ manager.add_item(item2)
 
 print("All items:")
 for item in manager.list_items():
-    print(item)
+    print(f"ID: {item.id[:8].upper()} | {item}")
+
+print("\nUpdate Status")
+update_id = input("Enter item ID to update: ")
+new_status = input("Enter new status: ")
+
+for item in manager.list_items():
+    if item.id.startswith(update_id):
+        manager.update_status(item.id, new_status)
+        print("Status updated")
+        break
+
+print("\nItems after update:")
+for item in manager.list_items():
+    print(f"ID: {item.id[:8].upper()} | {item}")
